b9f770a07e7eff0b17605827adb58fb1e6c45446
Tonye Jack <tonye@coherentpath.com>
2020-07-30 19:53:17 -0400

    Updated test.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,24 @@
 
 set -e
 
-FILES=$2
-#CURRENT_TAG=${GITHUB_REF/refs\/tags\//}
-#PREV_TAG=$(git describe --abbrev=0 --tags $(git rev-list --tags --skip=1  --max-count=1))
+if [[ $GITHUB_EVENT_NAME != 'release' ]]; then
+  echo "Skipping: This should only run on release.";
+  exit 0;
+fi
 
+FILES=$2
+NEW_TAG=${GITHUB_REF/refs\/tags\//}
+CURRENT_TAG=${3:-$(git describe --abbrev=0 --tags $(git rev-list --tags --skip=1  --max-count=1))}
+
+if [[ -z $CURRENT_TAG ]]; then
+  echo "Unable to determine where changes need to be updated."
+  echo "Please create a initial release before running this action."
+  exit 0;
+fi
 
 for path in ${FILES}
 do
-   echo "$path"
+   echo "Replacing $CURRENT_TAG with $NEW_TAG for: $path"
+   sed -i "s|$CURRENT_TAG|$NEW_TAG|g" "$path"
 done
 
