a7cb1aa07c0b7e5668672236dc0a81d797329afb
Tonye Jack <tonye@coherentpath.com>
2020-07-30 17:39:59 -0400

    Remove git stuff.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-CURRENT_TAG=${GITHUB_REF/refs\/tags\//}
-PREV_TAG=$(git describe --abbrev=0 --tags $(git rev-list --tags --skip=1  --max-count=1))
+#CURRENT_TAG=${GITHUB_REF/refs\/tags\//}
+#PREV_TAG=$(git describe --abbrev=0 --tags $(git rev-list --tags --skip=1  --max-count=1))
 
 
 for path in ${FILES}
