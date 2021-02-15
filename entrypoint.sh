a039531c3a43f84b47076218a761472187d79265
Tonye Jack <jtonye@ymail.com>
2021-02-15 13:36:24 -0500

    Update entrypoint.sh
    Closes: #25

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,16 +15,20 @@ NEW_TAG=${4:-"${GITHUB_REF/refs\/tags\//}"}
 PATTERN=$5
 
 if [[ -z $CURRENT_TAG ]]; then
-  echo "Unable to determine where changes need to be updated."
-  echo "Please create a initial release before running this action."
-  exit 0;
+  for path in ${FILES}
+  do
+     echo "Replacing $PATTERN with $NEW_TAG for: $path"
+     sed -i "s|$PATTERN|$PATTERN$NEW_TAG|g" "$path"
+  done
+
+  echo "::set-output name=new_version::$NEW_TAG"
+else
+  for path in ${FILES}
+  do
+     echo "Replacing $CURRENT_TAG with $NEW_TAG for: $path"
+     sed -i "s|$PATTERN$CURRENT_TAG|$PATTERN$NEW_TAG|g" "$path"
+  done
+
+  echo "::set-output name=new_version::$NEW_TAG"
+  echo "::set-output name=old_version::$CURRENT_TAG"
 fi
-
-for path in ${FILES}
-do
-   echo "Replacing $CURRENT_TAG with $NEW_TAG for: $path"
-   sed -i "s|$PATTERN$CURRENT_TAG|$PATTERN$NEW_TAG|g" "$path"
-done
-
-echo "::set-output name=new_version::$NEW_TAG"
-echo "::set-output name=old_version::$CURRENT_TAG"
