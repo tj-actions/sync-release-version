1f3f7b01a85a6735979bd8fd7fcd048e72d5cc75
Tonye Jack <jtonye@ymail.com>
2022-02-14 20:52:30 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,16 +7,27 @@ if [[ $GITHUB_EVENT_NAME != 'release' && -z "$4" ]]; then
   exit 0;
 fi
 
-TOKEN=$1
+SERVER_URL=$(echo "$GITHUB_SERVER_URL" | awk -F/ '{print $3}')
 
-git remote set-url origin "https://$TOKEN@github.com/$GITHUB_REPOSITORY"
+echo "Setting up 'temp_sync_release_version' remote..."
 
-git fetch origin +refs/tags/*:refs/tags/*
+git ls-remote --exit-code temp_sync_release_version 1>/dev/null 2>&1 && exit_status=$? || exit_status=$?
 
-FILES=$2
+if [[ $exit_status -ne 0 ]]; then
+  echo "No 'temp_sync_release_version' remote found"
+  echo "Creating 'temp_sync_release_version' remote..."
+  git remote remove temp_sync_release_version 2>/dev/null || true
+  git remote add temp_sync_release_version "https://${INPUT_TOKEN}@${SERVER_URL}/${GITHUB_REPOSITORY}"
+else
+  echo "Found 'temp_changed_files' remote"
+fi
+
+git fetch temp_sync_release_version +refs/tags/*:refs/tags/*
+
+FILES=$INPUT_FILES
 TAG=$(git describe --abbrev=0 --tags "$(git rev-list --tags --skip=1  --max-count=1 2>&1)" 2>&1) && exit_status=$? || exit_status=$?
 NEW_TAG=${4:-"${GITHUB_REF/refs\/tags\//}"}
-PATTERN=$5
+PATTERN=$INPUT_PATTERN
 
 if [[ $exit_status -ne 0 ]]; then
   echo "::warning::Initial release detected no updates would be made to specified files."
